fa646e4ee78a4386f9b648151e2964f8cbb8766e
Scott Wright <scott@wrightzone.com>
2026-02-01 14:04:09 -0500

    WIP Deobfuscating Variables

--- src/wordle.js
+++ src/wordle.js
@@ -179,7 +179,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
     vertical-align: middle;
     box-sizing: border-box;
     color: var(--tile-text-color);
-     /* text-transform: uppercase; */
+    text-transform: uppercase;
     user-select: none;
   }
 
@@ -320,8 +320,8 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
             }]), t
     }(c(HTMLElement));
     customElements.define("game-tile", v);
-    var w = document.createElement("template");
-    w.innerHTML = `
+    var gameRow = document.createElement("template");
+    gameRow.innerHTML = `
   <style>
     :host {
       display: block;
@@ -407,7 +407,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                 key: "connectedCallback",
                 value: function() {
                     var e = this;
-                    this.shadowRoot.appendChild(w.content.cloneNode(!0)), this.$row = this.shadowRoot.querySelector(".row");
+                    this.shadowRoot.appendChild(gameRow.content.cloneNode(!0)), this.$row = this.shadowRoot.querySelector(".row");
                     for (var a = function(a) {
                                 var s = document.createElement("game-tile"),
                                     t = e._letters[a];
@@ -1122,8 +1122,8 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
             }]), t
     }(c(HTMLElement));
     customElements.define("game-settings", _a);
-    var qa = document.createElement("template");
-    qa.innerHTML = `
+    var toastStyles = document.createElement("template");
+    toastStyles.innerHTML = `
   <style>
     .toast {
       position: relative;
@@ -1163,7 +1163,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                     key: "connectedCallback",
                     value: function() {
                         var e = this;
-                        this.shadowRoot.appendChild(qa.content.cloneNode(!0));
+                        this.shadowRoot.appendChild(toastStyles.content.cloneNode(!0));
                         var a = this.shadowRoot.querySelector(".toast");
                         a.textContent = this.getAttribute("text"), this._duration = this.getAttribute("duration") || 1e3, "Infinity" !== this._duration && setTimeout((function() {
                             a.classList.add("fade")
@@ -1272,8 +1272,8 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
     var Ka = document.createElement("template");
     // Ka.innerHTML = "\n  <style>\n  .toaster {\n    position: absolute;\n    top: 10%;\n    left: 50%;\n    transform: translate(-50%, 0);\n    pointer-events: none;\n    width: fit-content;\n  }\n  #game-toaster {\n    z-index: ".concat(1e3, ";\n  }\n  #system-toaster {\n    z-index: ").concat(4e3, ';\n  }\n\n  #game {\n    width: 100%;\n    max-width: var(--game-max-width);\n    margin: 0 auto;\n    height: 100%;\n    display: flex;\n    flex-direction: column;\n  }\n  header {\n    display: flex;\n    justify-content: space-between;\n    align-items: center;\n    height: var(--header-height);\n    color: var(--color-tone-1);\n    border-bottom: 1px solid var(--color-tone-4);\n  }\n  header .title {\n    font-weight: 700;\n    font-size: 36px;\n    letter-spacing: 0.2rem;\n    text-transform: uppercase;\n    text-align: center;\n    position: absolute;\n    left: 0;\n    right: 0;\n    pointer-events: none;\n  }\n\n  @media (max-width: 360px) {\n    header .title {\n      font-size: 22px;\n      letter-spacing: 0.1rem;\n    }\n  }\n\n  #board-container {\n    display: flex;\n    justify-content: center;\n    align-items: center;\n    flex-grow: 1;\n    overflow: hidden;\n  }\n  #board {\n    display: grid;\n    grid-template-rows: repeat(6, 1fr);\n    grid-gap: 5px;\n    padding:10px;\n    box-sizing: border-box;\n  }\n  button.icon {\n    background: none;\n    border: none;\n    cursor: pointer;\n    padding: 0 4px;\n  }\n\n  #debug-tools {\n    position: absolute;\n    bottom: 0;\n  }\n\n  </style>\n  <game-theme-manager>\n    <div id="game">\n      <header>\n        <div class="menu">\n          <button id="help-button" class="icon" aria-label="help">\n            <game-icon icon="help"></game-icon>\n          </button>\n        </div>\n        <div class="title">\n         WORDLE\n        </div>\n        <div class="menu">\n          <button id="statistics-button" class="icon" aria-label="statistics">\n            <game-icon icon="statistics"></game-icon>\n          </button>\n          <button id="settings-button" class="icon" aria-label="settings">\n            <game-icon icon="settings"></game-icon>\n          </button>\n        </div>\n      </header>\n        <div id="board-container">\n          <div id="board"></div>\n        </div>\n        <game-keyboard></game-keyboard>\n        <game-modal></game-modal>\n        <game-page></game-page>\n        <div class="toaster" id="game-toaster"></div>\n        <div class="toaster" id="system-toaster"></div>\n    </div>\n  </game-theme-manager>\n  <div id="debug-tools"></div>\n');
     Ka.innerHTML = document.getElementById('header-container').innerHTML;
-    var Qa = document.createElement("template");
-    Qa.innerHTML = `
+    var qaButtons = document.createElement("template");
+    qaButtons.innerHTML = `
 <button id="reveal">reveal</button>
 <button id="shake">shake</button>
 <button id="bounce">bounce</button>
@@ -1485,7 +1485,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                     key: "debugTools",
                     value: function() {
                         var e = this;
-                        this.shadowRoot.getElementById("debug-tools").appendChild(Qa.content.cloneNode(!0)), this.shadowRoot.getElementById("toast").addEventListener("click", (function(a) {
+                        this.shadowRoot.getElementById("debug-tools").appendChild(qaButtons.content.cloneNode(!0)), this.shadowRoot.getElementById("toast").addEventListener("click", (function(a) {
                             e.addToast("hello world")
                         })), this.shadowRoot.getElementById("modal").addEventListener("click", (function(a) {
                             var s = e.$game.querySelector("game-modal");
@@ -1703,9 +1703,11 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
 `;
     var is = document.createElement("template");
     is.innerHTML = `<button>key</button>`;
-    var ls = document.createElement("template");
-    ls.innerHTML = `<div class="spacer"></div>`;
-    var ds = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"], ["-", "a", "s", "d", "f", "g", "h", "j", "k", "l", "-"], ["↵", "z", "x", "c", "v", "b", "n", "m", "←"]],
+    var spacerDiv = document.createElement("template");
+    spacerDiv.innerHTML = `<div class="spacer"></div>`;
+    var keyLabels = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+        ["-", "a", "s", "d", "f", "g", "h", "j", "k", "l", "-"],
+        ["↵", "z", "x", "c", "v", "b", "n", "m", "←"]],
         us = function(e) {
             r(t, e);
             var a = h(t);
@@ -1748,7 +1750,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                         })), this.$keyboard.addEventListener("transitionend", (function(a) {
                             var s = a.target.closest("button");
                             s && e.$keyboard.contains(s) && s.classList.remove("fade")
-                        })), ds.forEach((function(a) {
+                        })), keyLabels.forEach((function(a) {
                             var s = document.createElement("div");
                             s.classList.add("row"), a.forEach((function(e) {
                                 var a;
@@ -1758,7 +1760,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                                         t.setAttribute("icon", "backspace"), a.textContent = "", a.appendChild(t), a.classList.add("one-and-a-half")
                                     }
                                     "↵" == e && (a.textContent = "enter", a.classList.add("one-and-a-half"))
-                                } else (a = ls.content.cloneNode(!0).firstElementChild).classList.add(1 === e.length ? "half" : "one");
+                                } else (a = spacerDiv.content.cloneNode(!0).firstElementChild).classList.add(1 === e.length ? "half" : "one");
                                 s.appendChild(a)
                             })), e.$keyboard.appendChild(s)
                         })), this._render()
@@ -1963,25 +1965,32 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
             }))
         }))
     }
-    function As(e, a, s) {
+    // Share results via native share API or fall back to clipboard
+    async function shareOrCopy(data, onSuccess, onError) {
         try {
-            t = navigator.userAgent || navigator.vendor || window.opera, !/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(t) && !/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(t.substr(0, 4)) || navigator.userAgent.toLowerCase().indexOf("firefox") > -1 || void 0 === navigator.share || !navigator.canShare || !navigator.canShare(e) ? function(e) {
-                return cs(this, void 0, void 0, (function() {
-                    return ps(this, (function(a) {
-                        if (ks) return [2, ks(e)];
-                        if (!Es(function(e) {
-                                    var a = {};
-                                    return a["text/plain"] = e, a
-                                }(e)))
-                            throw new Error("writeText() failed");
-                        return [2]
-                    }))
-                }))
-            }(e.text).then(a, s) : navigator.share(e)
-        } catch ( e ) {
-            s()
-        } var t
+            // Try native share if available and supported
+            if (navigator.canShare?.(data)) {
+                await navigator.share(data);
+                onSuccess();
+                return;
+            }
+        } catch (err) {
+            // User cancelled share or share failed - fall through to clipboard
+            if (err.name === 'AbortError') {
+                // User cancelled - don't show error, just return
+                return;
+            }
+        }
+
+        // Clipboard fallback
+        try {
+            await navigator.clipboard.writeText(data.text);
+            onSuccess();
+        } catch (err) {
+            onError();
+        }
     }
+
     var Cs = document.createElement("template");
     Cs.innerHTML = `
   <style>
@@ -2233,7 +2242,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
                             var p = this.shadowRoot.querySelector(".footer"),
                                 m = Is.content.cloneNode(!0);
                             p.appendChild(m), this.shadowRoot.querySelector("button#share-button").addEventListener("click", (function(a) {
-                                a.preventDefault(), a.stopPropagation();As(function(e) {
+                                a.preventDefault(), a.stopPropagation();shareOrCopy(function(e) {
                                     var a = e.evaluations,
                                         s = e.dayOffset,
                                         t = e.rowIndex,
